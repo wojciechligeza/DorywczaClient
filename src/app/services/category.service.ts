bd2e1f910c32c56c6ff0e94051df9fee8d1fb0d7
Wojciech <wojciech.ligeza@gmail.com>
2020-01-22 14:22:29 +0100

    clearCache

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { ICategory } from '../interfaces/icategory';
 import { Observable } from 'rxjs';
 import { environment } from '@environments/environment';
+import { publishReplay, refCount } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +12,24 @@ export class CategoryService {
 
   constructor(private http: HttpClient) { }
 
+  public categoriesCache: Observable<Array<ICategory>>;
+
   getCategories(): Observable<Array<ICategory>> {
-    return this.http.get<Array<ICategory>>(`${environment._baseURL}/api/Categories`);
+    if (!this.categoriesCache) {
+      this.categoriesCache = this.http.get<Array<ICategory>>(`${environment._baseURL}/api/Categories`)
+                            .pipe(
+                              publishReplay(1),
+                              refCount()
+                            );
+    }
+    return this.categoriesCache;
   }
 
   getCategory(id: number): Observable<ICategory> {
     return this.http.get<ICategory>(`${environment._baseURL}/api/Categories/${id}`);
+  }
+
+  clearCache(): void {
+    this.categoriesCache = null;
   }
 }
